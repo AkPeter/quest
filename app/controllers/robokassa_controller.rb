8661a4867d1c4c4452b300239055776f7d215522
nik <shadows.of.unevenness@gmail.com>
2015-07-23 19:27:19 +0300

    робокасса

--- app/controllers/robokassa_controller.rb
+++ app/controllers/robokassa_controller.rb
@@ -25,30 +25,40 @@ class RobokassaController < ApplicationController
 
   def paid_confirmed
     # пользователь оплатил в робокассе, надо сверить что он там оплатил
-    ticket = Ticket.find(session[:tid])
+    ticket = Ticket.find(current_ticket)
 
     out_sum = ticket.price.to_f
     inv_id = ticket.id.to_i
 
+    render text: "OK#{inv_id}"
+
+=begin
     if params[:OutSum].to_f >= out_sum  && params[:InvId].to_i == inv_id && params[:SignatureValue] == Digest::MD5.new << "#{out_sum}:#{inv_id}:#{Rails.application.secrets.robokassa_password2}"
       #котирую оплату пользователем и я говорю Окей .)
-      render text: ticket.update(ticket_status_id: 3) ? "OK#{inv_id}" : 'SHITHAPPENS'
+      if ticket.update(ticket_status_id: 3)
+        render text: "OK#{inv_id}"
+      else
+        render text: 'SHITHAPPENS'
+      end
     else
       # 8======>
-      # резерв снимается по истечении 17 минут, но не тут
+      # резерв снимается по истечении времени резерва, но не тут
       render text: 'SHITHAPPENS'
     end
+=end
   end
 
   def transaction_confirmed
     # всё прошло успешно - можно потрепать пользователя по плечу
-    @ticket = current_ticket
-    session[:tid] = nil
-    UserMailer.ticket_purchased(@ticket).deliver_now
+    if current_ticket.id == 3
+      @ticket = current_ticket
+      session[:tid] = nil
+      UserMailer.ticket_purchased(@ticket).deliver_now
 
-    # снаряжаем фоновую задачу правильным образом ! ну и рассылка будет если ещё не поздно рассылать )
-    time2remind = @ticket.dt.to_datetime - TicketsController::UserRemindBefore
-    TicketUserRemindJob.set(wait_until: time2remind).perform_later(@ticket.id, current_user.id) if DateTime.now < time2remind
+      # снаряжаем фоновую задачу правильным образом ! ну и рассылка будет если ещё не поздно рассылать )
+      time2remind = @ticket.dt.to_datetime - TicketsController::UserRemindBefore
+      TicketUserRemindJob.set(wait_until: time2remind).perform_later(@ticket.id, current_user.id) if DateTime.now < time2remind
+    end
 
   end
 
