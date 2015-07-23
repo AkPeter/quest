5abec4aec494a32a39824162ad2b34bd28f95c12
petr <pa@dwsol.ru>
2015-07-23 13:24:19 +0300

    Добавлены плюшки в четверг + фона эффект

---
+++ app/assets/javascripts/parallax.js
@@ -0,0 +1,13 @@
+/**
+ * Created by petr on 23.07.15.
+ */
+
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+});
+
+function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('#bg-img').css('top',(0-(scrolled*.45))+'px');
+    $('#bg-fon').css('top',(0-(scrolled*.45))+'px');
+}
