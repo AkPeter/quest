16f82163116955fc26f1c32842c5bbf3328ba5dd
nik <shadows.of.unevenness@gmail.com>
2015-07-24 07:29:37 +0300

    смски

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,12 @@
 class UserMailer < ApplicationMailer
+  include SmsLib
 
   def new_bee(user, password)
     @username = user.name
     @signin  = ApplicationController::SignInURL
     @password = password
 
-    SmsController.new_bee user, password if user.phone
+    sms_new_bee user, password if user.phone
 
     mail(to: user.email, subject: "С подключением на #{ApplicationController::Domain} !")
   end
@@ -15,7 +16,7 @@ class UserMailer < ApplicationMailer
     @signin  = ApplicationController::SignInURL
     @new_password = user, new_password
 
-    SmsController.password_reset new_password if user.phone
+    sms_password_reset user, new_password if user.phone
 
     mail(to: user.email, subject: "Новый пароль для входа на #{ApplicationController::Domain}")
   end
@@ -25,7 +26,7 @@ class UserMailer < ApplicationMailer
     @ticket = ticket
     @reserve_time = RobokassaController::RESERVE_TIME
 
-    SmsController.ticket_reserved ticket if ticket.user.phone
+    sms_ticket_reserved ticket if ticket.user.phone
 
     mail(to: ticket.user.email, subject: "Резерв билета.")
   end
@@ -33,7 +34,7 @@ class UserMailer < ApplicationMailer
   def ticket_purchased(ticket)
     @ticket = ticket
 
-    SmsController.ticket_purchased ticket if ticket.user.phone
+    sms_ticket_purchased ticket if ticket.user.phone
 
     mail(to: ticket.user.email, subject: "Ты приобрёл билет !")
   end
@@ -41,7 +42,7 @@ class UserMailer < ApplicationMailer
   def ticket_remind(ticket)
     @ticket = ticket
 
-    SmsController.ticket_remind ticket if ticket.user.phone
+    sms_ticket_remind ticket if ticket.user.phone
 
     mail(to: ticket.user.email, subject: "Скоро твой квест ! Не пропусти !")
   end
