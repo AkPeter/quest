b80001edf1a6a7614f5e30f514a6160a35d5cbbe
nik <shadows.of.unevenness@gmail.com>
2015-07-25 14:39:28 +0300

    подробные сообщения от робокассы

--- app/controllers/robokassa_controller.rb
+++ app/controllers/robokassa_controller.rb
@@ -5,24 +5,31 @@ class RobokassaController < ApplicationController
   RESERVE_TIME = 15.minutes
 
   def prepay
-    @ticket = current_ticket
-    session[:tid] = @ticket.id
-    unless @ticket
-      redirect_to root_url
-    else
-      out_sum = @ticket.price.to_f
-      inv_id = @ticket.id.to_i
+    if current_ticket
 
-      @params = {
-          :mrch_login => Rails.application.secrets.robokassa_mrch_login,
-          :out_sum => out_sum,
-          :inv_id => inv_id,
-          :desc => "#{@ticket.quest_item.name} начало #{l(@ticket.dt.to_datetime, :format => "%d %B %A %H:%M")}",
-          :password1 => Rails.application.secrets.robokassa_password1,
-          :signature_value => Digest::MD5.new << "#{Rails.application.secrets.robokassa_mrch_login}:#{out_sum}:#{inv_id}:#{Rails.application.secrets.robokassa_password1}",
-          :culture => 'ru'
-      }
+      @ticket = current_ticket
+      session[:tid] = @ticket.id
+      unless @ticket
+        redirect_to root_url
+      else
+        out_sum = @ticket.price.to_f
+        inv_id = @ticket.id.to_i
+
+        @params = {
+            :mrch_login => Rails.application.secrets.robokassa_mrch_login,
+            :out_sum => out_sum,
+            :inv_id => inv_id,
+            :desc => "#{@ticket.quest_item.name} начало #{l(@ticket.dt.to_datetime, :format => "%d %B %A %H:%M")}",
+            :password1 => Rails.application.secrets.robokassa_password1,
+            :signature_value => Digest::MD5.new << "#{Rails.application.secrets.robokassa_mrch_login}:#{out_sum}:#{inv_id}:#{Rails.application.secrets.robokassa_password1}",
+            :culture => 'ru'
+        }
+      end
+
+    else
+      redirect_to root_url, notice: 'резерв билета истёк'
     end
+
 
   end
 
